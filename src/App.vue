9428bc095d6608f0af427bb757482feed99b3f38
HumbleBoyy <asadullohabdurakhmon1.1@gmail.com>
2025-04-23 23:02:01 +0500

    first commit

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+<script>
+   export default {
+    data(){
+      return {
+        name: "John Doe",
+        status:false,
+        tasks:['task one', 'task two', 'task three'],
+        link:"/https"
+      }
+    }
+   }
+</script>
+
+<template>
+  <h1>{{ name }}</h1>
+  <p v-if="status === true">User is active</p>
+  <p v-else>User is not active</p>
+
+  <h3>Tasks</h3>
+  <ul>
+    <li v-for="task in tasks" :key="index">{{ task }}</li>
+  </ul>
+</template>
+
